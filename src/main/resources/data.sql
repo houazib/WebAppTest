285ecbca2f784cff0fef8c0ab16e283d0f7a0998
Hafid Ouazib <houazib@oaciq.com>
2018-02-18 11:17:14 -0500

    add h2 properties and sql data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+
+insert into AGENCY VALUES(1,'Agence montréal', 'Montréal, Québec, Canada');
+insert into AGENCY VALUES(2,'Agence Brossard', 'Brossard, Québec, Canada');
+
+insert into CLIENT VALUES(1, 'client@test.com', 'Rambo', 'puissant', 'Homme', 1);
+insert into CLIENT VALUES(2, 'client@test.com', 'John', 'Doe', 'Homme', 2);
+
+insert into ACCOUNT VALUES(1, '123edws56k', 1);
+insert into ACCOUNT VALUES(2, 'jkh67534ju', 2);
